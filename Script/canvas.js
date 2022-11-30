c042e79f561700177faf8a7eb4b9a5ffbcabb5a4
Gabrielcastro1800 <118899849+Gabrielcastro1800@users.noreply.github.com>
2022-11-30 10:35:53 -0300

    4w2t

--- Script/canvas.js
+++ Script/canvas.js
@@ -4046,11 +4046,11 @@ function draw(){
         }
         if(mspeed>ispeed && click==1 || ispeed>mspeed && click==3){
             damagepng.src = "Images/fundo/damage.png";
-            ctx.drawImage(damagepng,50,220,50,50);
+            ctx.drawImage(damagepng,80,240,50,50);
         }
         if(ispeed>mspeed && click==1 || mspeed>ispeed && click==3){
             damagepng.src = "Images/fundo/damage.png";
-            ctx.drawImage(damagepng,50,220,50,50);
+            ctx.drawImage(damagepng,80,240,50,50);
         }
     }
     if(tela == 4){

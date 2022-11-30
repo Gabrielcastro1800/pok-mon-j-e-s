4299d682b8f550a571263dea593e34a25ad5cd2e
Everabbit <118900053+Everabbit@users.noreply.github.com>
2022-11-30 14:55:10 -0300

    a

--- Script/canvas.js
+++ Script/canvas.js
@@ -4001,7 +4001,7 @@ function draw(){
 
         //desenhar contagem
         backcount.src="Images/fundo/counter.png";
-        ctx.drawImage(backcount, 385, -5, 30, 30);
+        ctx.drawImage(backcount, 350, -5, 80, 80);
 
         //desenhar barras de vida
         ctx.drawImage(chat, 10, 15, 240, 60);
